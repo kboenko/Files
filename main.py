d39838c6d0844c3622308d3f58b090ee8ca34e77
kostyawh <kostyawh@gmail.com>
2018-10-13 13:47:38 +0700

    add three functions

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
 import os
 import shutil
+from random import randint
+
+MAIN_PATH = '/home/kostyawh/files/'
+EXTENSION = '.txt'
+
+def sum_file(path):
+    arr = []
+    with open(path, 'rt') as fin:
+        string = fin.readline()
+        while string != '':
+            arr.append(int(string))
+            string = fin.readline()
+    return sum(arr)
+
+def create_files():
+    for i in range(1,11):
+        filename = MAIN_PATH + str(i) + EXTENSION
+        with open(filename, 'wt') as fout:
+            for j in range(3):
+                fout.write(str(randint(1,100)) + '\n')
+
+def sum_files():
+    files = []
+    sums = []
+
+    for i in range(2):
+        files.append(MAIN_PATH + str(randint(1,10)) + EXTENSION)
+
+    print(files)
+
+    for file in files:
+        if os.path.exists(file):
+          sums.append(sum_file(file))
+
+    return sum(sums)
+
 
 def return_dir_info(path):
 
@@ -41,3 +77,7 @@ print(b)
 path2 = input('Type another path: ')
 
 remove_dir(path2)
+
+create_files()
+
+print(sum_files())
